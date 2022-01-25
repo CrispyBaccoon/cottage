6fc7fdbff09730b105a6d111071c2cefa5d4d3bc
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-01-25 19:56:43 +0100

    Fixed internal Link

--- components/lib/Link.tsx
+++ components/lib/Link.tsx
@@ -1,15 +1,17 @@
-import * as link from "next/link";
+import * as IntLink from "next/link";
 
 interface LinkProps {
   Name?: string;
   Ref: string;
 }
 
+const LinkStyling: string = "text-[#D19600]";
+
 export default function Link(props: LinkProps) {
   return (
     <a
       // className="text-gray-700 underline underline-offset-1 hover:bg-slate-200"
-      className=" text-[#D19600]"
+      className={LinkStyling}
       href={props.Ref}
     >
       {props.Name ? props.Name : props.Ref}
@@ -19,8 +21,8 @@ export default function Link(props: LinkProps) {
 
 export function InternalLink(props: LinkProps) {
   return (
-    <link ref={`/${props.Ref}`}>
-      <Link Ref="#" Name={props.Name ? props.Name : props.Ref} />
-    </link>
+    <IntLink.default href={props.Ref}>
+      <div className={LinkStyling}>{props.Name ? props.Name : props.Ref}</div>
+    </IntLink.default>
   );
 }
