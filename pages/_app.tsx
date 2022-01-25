eda1907bfe825162dfdb73e3fa8e49c4df4c8093
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-01-25 17:01:19 +0100

    added empty navigationbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import "../styles/globals.scss";
 import { AppProps } from "next/app";
+import NavigationBar from "../components/NavigationBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="w-full h-full bg-[#F7F1E3] flex flex-col md:flex-row">
-      <div></div>
+      <div>
+        <NavigationBar />
+      </div>
       <Component {...pageProps} />
     </div>
   );
