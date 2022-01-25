51cc1b867f3e2571ec98ec65ab61929aff071740
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-01-25 19:10:07 +0100

    converted header to typescript

--- components/lib/Header.tsx
+++ components/lib/Header.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import { ReactChild } from "react";
 
-export function Title(props) {
+interface HeaderProps {
+  children: ReactChild;
+}
+
+export function Title(props: HeaderProps) {
   return (
     <div className="mx-auto w-fit font-semibold text-[#4F321C] text-3xl sm:text-4xl mb-6 md:mb-6">
       {props.children}
@@ -8,7 +12,7 @@ export function Title(props) {
   );
 }
 
-export function SubTitle(props) {
+export function SubTitle(props: HeaderProps) {
   return (
     <div className="max-x-auto w-10/12 font-normal text-center text-[#8C704F] text-lg sm:text-xl mb-6 md:mb-6">
       {props.children}
@@ -16,7 +20,7 @@ export function SubTitle(props) {
   );
 }
 
-export function MainHeader(props) {
+export function MainHeader(props: HeaderProps) {
   return (
     <div className="font-semibold text-[#4F321C] text-3xl sm:text-2xl my-4">
       {props.children}
@@ -24,7 +28,7 @@ export function MainHeader(props) {
   );
 }
 
-export function Header(props) {
+export function Header(props: HeaderProps) {
   return (
     <div className="font-semibold text-[#4F321C] text-2xl sm:text-xl my-4">
       {props.children}
