923a576501d0bbda6e919765250fd987d145dc63
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-01-25 19:20:46 +0100

    converted Paragraph to typescript

---
+++ components/lib/Paragraph.tsx
@@ -0,0 +1,13 @@
+import { ReactChild } from "react";
+
+interface ParagraphProps {
+  children: ReactChild;
+}
+
+export default function Paragraph(props: ParagraphProps) {
+  return (
+    <p className="font-serif text-xl sm:text-lg text-[#4F321C] my-2">
+      {props.children}
+    </p>
+  );
+}
