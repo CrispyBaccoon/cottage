aa80b07bd7c5be7abd207cb347d54017263bc3d1
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-01-25 17:22:58 +0100

    added styling to navigationbar

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,3 +1,7 @@
 export default function NavigationBar() {
-  return <div></div>;
+  return (
+    <div className="mt-6 md:mt-8 mx-5 md:mx-8 my-5 md:my-4">
+      <ul className="list-none"></ul>
+    </div>
+  );
 }
