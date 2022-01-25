4efe4e7bda8def68a0f8e87ffeb78137b636aca8
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-01-25 16:59:26 +0100

    Added styling to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,12 @@ import "../styles/globals.scss";
 import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="w-full h-full bg-[#F7F1E3] flex flex-col md:flex-row">
+      <div></div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
