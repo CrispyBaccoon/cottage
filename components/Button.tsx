90a2734809eadcbac0ee5d6eb7325c8b33a68609
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-01-25 16:55:31 +0100

    styled button with tailwind

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,3 @@
-import styles from "./Button.module.scss";
-
 interface ButtonProps {
   Name: String;
   OnClick: () => any;
@@ -7,7 +5,10 @@ interface ButtonProps {
 
 export default function Button(props: ButtonProps) {
   return (
-    <button onClick={props.OnClick}>
+    <button
+      className="min-w-[1.5rem] w-fit min-h-[1rem] h-fit px-3 py-2 bg-blue-500 text-white rounded-full"
+      onClick={props.OnClick}
+    >
       <div>{props.Name}</div>
     </button>
   );
