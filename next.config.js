35e76a52b3b809b6fa14f281c6526816002fb2b6
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-02-13 20:52:00 +0100

    added asset prefix on production

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const path = require("path");
 
+const isProd = process.env.NODE_ENV == "production";
+
 module.exports = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  assetPrefix: isProd ? "/cottage/" : "",
 };
